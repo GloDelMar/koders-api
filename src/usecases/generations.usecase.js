d68b6c0f231fb7b9b5dcbe644d64d053dc352301
Gloriela Suarez <glo.suacas@gmail.com>
2024-06-05 21:33:22 -0600

    actual

--- src/usecases/generations.usecase.js
+++ src/usecases/generations.usecase.js
@@ -1,4 +1,34 @@
-const creatError = require("http-errors")
-const Generations = require("../models/generations.model")
-const encrypt = require("../lib/encrypt")
+const Generations = require('../models/generations.model');
 
+async function createGeneretion (generationData) {
+    const newGeneration = await Generations.create(generationData);
+    return newGeneration;
+};
+
+async function getAllGenerations () {
+    const allGenerations = await Generations.find();
+    return allGenerations;
+};
+
+async function getGenerationById (id) {
+    const generationById = await Generations.findById(id);
+    return generationById;
+};
+
+async function deleteGenerationById (id) {
+    const generationDeleted = await Generations.findByIdAndDelete(id);
+    return generationDeleted;
+};
+
+async function updateGenerationById (id, newData) {
+    const generationUpdated = await Generations.findByIdAndUpdate(id, newData, {new: true});
+    return generationUpdated;
+};
+
+module.exports = {
+    createGeneretion,
+    getAllGenerations,
+    getGenerationById,
+    deleteGenerationById,
+    updateGenerationById
+};
