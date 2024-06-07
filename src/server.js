4c984f9699ddfdd6c632bd6af44d53662ce7ab86
Gloriela Suarez <glo.suacas@gmail.com>
2024-06-06 19:08:45 -0600

    no se que le movi

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ const cors= require("cors")
 const express = require("express")
 
 const kodersRouter = require("./routes/koders.router")
+const generationsRouter = require("./routes/generations.router")
 const authRouter = require("./routes/auth.router")
+
 
 const app = express()
 
@@ -11,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/koders", kodersRouter)
+app.use("/generations", generationsRouter)
 app.use("/auth", authRouter)
 
 app.get("/", (request, response)=>{
